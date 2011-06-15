97f650a28f67b6d37f3478c758d36313cf82b2d6
Cameron Westland <cameron@bigbangtechnology.com>
2011-06-14 21:08:29 -0400

    Implemented count around function to see how many active items are in the cell

--- public/javascripts/models/grid.js
+++ public/javascripts/models/grid.js
@@ -1,27 +1,93 @@
 var Grid = function(width, height) {
+  var dataSource = [];
+  var nextDataSource = [];
+
   function createDataSource() {
-    var data = [];
-    
-    //create rows 
-    for (var i=0; i < height; i+=1) {    
-      data[i] = [];
+      var data = [];
       
-      evenRow = i % 2;
+      //create rows 
+      for (var i=0; i < height; i+=1) {    
+        data[i] = [];
+        
+        for (var j=0; j < width; j+=1) {
+          data[i][j] = false;          
+        }      
+      }
+      
+      return data;
+  }
+  
+  function calculate() {
+    nextDataSource = [];
+    
+    for (var i=0; i < height; i+=1) {    
+      nextDataSource[i] = [];
       
       for (var j=0; j < width; j+=1) {
-        if (evenRow) {
-          data[i][j] = (j%2) ? true : false;          
-        } else {
-          data[i][j] = (j%2) ? false : true;          
+        count = countAround(i,j);
+        
+        // do some logic on the count and set the data
+        if (count == 3) {
+          nextDataSource[i][j] = true;
         }
       }      
-    }    
-    return data;
+    }  
   }
-
-  var dataSource = createDataSource();
   
-  return {
+  // show the number of active items surrounding the row and column
+  function countAround(row, column) {
+    function up() { row -= 1 }
+    
+    function right() { column += 1 }
+    
+    function down() { row += 1 }
+    
+    function left() { column -= 1 }
+        
+    var count = 0;
+
+    up();
+    
+    count += public.getCell(row, column);
+
+    right();
+    
+    count += public.getCell(row, column);
+    
+    down();
+    
+    count += public.getCell(row, column);
+    
+    down();
+    
+    count += public.getCell(row, column);
+    
+    left();
+    
+    count += public.getCell(row, column);
+        
+    left();
+    
+    count += public.getCell(row, column);
+    
+    up();
+
+    count += public.getCell(row, column);    
+    
+    up();
+    
+    count += public.getCell(row, column);
+    
+    return count;
+  }
+  
+  function activate() {
+    dataSource = nextDataSource;
+  }
+  
+  dataSource = createDataSource();
+  
+  var public = {
     data: function(){
       return dataSource;
     },
@@ -33,7 +99,7 @@ var Grid = function(width, height) {
         }
       }
     },
-    
+
     rows: function() {
       return {
         length: function() {
@@ -43,15 +109,23 @@ var Grid = function(width, height) {
     },
     
     getCell: function(row, column) {
-      return dataSource[row][column];
+      if (typeof dataSource[row] == "undefined") 
+        return false;
+      
+      return (dataSource[row][column] == true) ? true : false;
+    },
+
+    setCell: function(row, column, value) {
+      dataSource[row][column] = value;
+    },
+    
+    iterate: function() {
+      calculate();
+      activate();
     }
   }
+  
+  return public;
 }
 
 // TODO: throw error when there are no rows
-
-[
-  [true,    true,   true],
-  [false,   false,  false],
-  [true,    true,   true]
-]
--- test/grid_test.js
+++ test/grid_test.js
@@ -14,6 +14,19 @@ test("when I build a 20x20 grid, it should have 400 cells", function() {
 test("when I build a 3x3 grid, the cells should be activated in a checkerboard fashion", function() {
   var sut = new Grid(3,3);
   
+  // set up a fun checkered test:
+  for (var i=0; i < sut.rows().length(); i+=1) {    
+    var evenRow = i % 2;
+    
+    for (var j=0; j < sut.columns().length(); j+=1) {
+      if (evenRow) {
+        sut.setCell(i,j, (j%2) ? true : false);
+      } else {
+        sut.setCell(i,j, (j%2) ? false : true);
+      }
+    }      
+  }    
+  
   equals(true,  sut.getCell(0,0));
   equals(false, sut.getCell(0,1));
   equals(true,  sut.getCell(0,2));
@@ -24,3 +37,18 @@ test("when I build a 3x3 grid, the cells should be activated in a checkerboard f
   equals(false, sut.getCell(2,1));
   equals(true,  sut.getCell(2,2));
 });
+
+test("An empty tile with exactly three tiles surrounding it will become alive.", function() {
+  var sut = new Grid(3,2);
+  
+  sut.setCell(0,0, true);
+  sut.setCell(0,1, true);
+  sut.setCell(0,2, true);
+  
+  // do something
+  sut.iterate();
+  
+  equals(sut.getCell(1,1), true);
+  
+});
+
